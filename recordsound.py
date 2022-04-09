df6135fdbe11e2762bae43e1a56451e5d2f8a560
ZAHHAKK <103303342+ZAHHAKK@users.noreply.github.com>
2022-04-09 15:35:08 +0430

    Delete recordsound.py

--- recordsound.py
+++
@@ -1,6 +0,0 @@
-import ctypes
-ctypes.windll.winmm.mciSendStringW("open new Type waveaudio Alias recsound", "", 0, 0)
-ctypes.windll.winmm.mciSendStringW("record recsound", "", 0, 0)
-input()
-ctypes.windll.winmm.mciSendStringW("save recsound C:\\record.wav", "", 0, 0)
-ctypes.windll.winmm.mciSendStringW("close recsound ", "", 0, 0)
